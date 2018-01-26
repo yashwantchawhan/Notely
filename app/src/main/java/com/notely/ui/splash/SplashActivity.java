21931f4a97bec6e58dcdc6ae93925e2d508763e5
yashwant_bytemark <yashwant@bytemark.co>
2018-01-27 02:13:24 +0530

    Added flash screen

---
+++ app/src/main/java/com/notely/ui/splash/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.notely.ui.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.notely.R;
+import com.notely.ui.list.ListNotesActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, ListNotesActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
