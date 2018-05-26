5ba03e924c8c3d52acd20ce66dadd5b88bc102e1
yashwantchawhan <ynchawhan4@gmail.com>
2018-05-27 01:39:20 +0800

    no message

--- app/src/main/java/com/notely/utility/ItemTouchHelperAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.notely.utility;
-
-/**
- * Created by yashwant on 23/01/18.
- */
-
-public interface ItemTouchHelperAdapter {
-    void onItemDismiss(int position);
-}
---
+++ app/src/main/java/com/notely/utility/ItemTouchHelperAdapter.kt
@@ -0,0 +1,9 @@
+package com.notely.utility
+
+/**
+ * Created by yashwant on 23/01/18.
+ */
+
+interface ItemTouchHelperAdapter {
+    fun onItemDismiss(position: Int)
+}
